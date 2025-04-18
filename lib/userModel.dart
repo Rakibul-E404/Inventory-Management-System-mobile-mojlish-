fd9cbc890580b829c4f9620581074c0021103fce
Rakibul-E404 <124011903+Rakibul-E404@users.noreply.github.com>
2025-04-19 01:09:49 +0600

    Update userModel.dart

--- lib/userModel.dart
+++ lib/userModel.dart
@@ -1,24 +1,4 @@
-// class User {
-//   final String username;
-//   final String password;
-//   final String role;
-//
-//   User({required this.username, required this.password, required this.role});
-// }
-//
-// final List<User> users = [
-//   // User(username: 'admin', password: 'admin123', role: 'admin'),
-//   // User(username: 'moderator', password: 'mod123', role: 'moderator'),
-//   User(username: 'a', password: 'a1', role: 'admin'),
-//   User(username: 'm', password: 'm1', role: 'moderator'),
-// ];
 
-
-///
-///
-///
-///
-///
 // userModel.dart
 class User {
   final String username;
